d1fe43206887f6d35c4e5faacf87e4ff2681e847
weiss <kingbaiing@163.com>
2023-02-26 17:09:27 +0100

    let filter option ignore empty string

--- src/clean.rs
+++ src/clean.rs
@@ -1,6 +1,5 @@
 use std::{ffi::OsStr, path::Path};
 
-
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 
@@ -22,29 +21,38 @@ pub struct FilterRule {
     pub name_suffix: Option<String>,
 }
 
+fn try_unwrap(c: &Option<String>) -> Option<&str> {
+    match c {
+        Some(x) if !x.is_empty() => Some(x),
+        _ => None,
+    }
+}
+
 impl FilterRule {
     fn is_matched(&self, file_path: &str) -> bool {
         let p = Path::new(file_path);
-        if let Some(dir_prefix) = &self.dir_prefix {
+        if let Some(dir_prefix) = try_unwrap(&self.dir_prefix) {
             if !p.starts_with(Path::new(dir_prefix)) {
                 return false;
             }
         }
 
-        if let Some(ext) = &self.ext {
+        if let Some(ext) = try_unwrap(&self.ext) {
             if !(p.extension() == Some(OsStr::new(ext))) {
                 return false;
             }
         }
 
-        if self.name_prefix.is_some() | self.name_suffix.is_some() {
+        let prefix = try_unwrap(&self.name_prefix);
+        let suffix = try_unwrap(&self.name_suffix);
+        if prefix.is_some() | suffix.is_some() {
             if let Some(Some(file_name)) = p.file_name().map(|x| x.to_str()) {
-                if let Some(name_prefix) = &self.name_prefix {
+                if let Some(name_prefix) = prefix {
                     if !file_name.starts_with(name_prefix) {
                         return false;
                     }
                 }
-                if let Some(name_suffix) = &self.name_suffix {
+                if let Some(name_suffix) = suffix {
                     if !file_name.ends_with(name_suffix) {
                         return false;
                     }
